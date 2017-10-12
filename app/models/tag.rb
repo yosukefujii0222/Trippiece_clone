edbf8577ea4d0990ec43a56c5510cff9e4fee0f8
yosukefujii0222 <look.in.my.eyes@softbank.ne.jp>
2017-10-12 15:15:54 +0900

    Delete tags-database

--- app/models/tag.rb
+++
@@ -1,4 +0,0 @@
-class Tag < ActiveRecord::Base
-  has_many :recommend_tags
-  has_many  :recommends, through: :recommend_tags
-end
--- db/migrate/20170929085138_create_recommend_tags.rb
+++
@@ -1,9 +0,0 @@
-class CreateRecommendTags < ActiveRecord::Migration
-  def change
-    create_table :recommend_tags do |t|
-      t.integer :recommnd_id, null: false, foreign_key: true
-      t.integer :tag_id, null: false, foreign_key: true
-      t.timestamps null: false
-    end
-  end
-end
