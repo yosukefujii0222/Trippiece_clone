2e38cba52594978286fd317cc0f927a9cc3aa7ad
yosukefujii0222 <look.in.my.eyes@softbank.ne.jp>
2017-10-12 15:16:58 +0900

    Create devise-model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,14 @@
 class User < ActiveRecord::Base
   has_many  :recommends
+  has_many  :favorites
+  has_many  :recommends, through: :favorites
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_attached_file :avatar,
+                      styles:  { medium: "300x300#", thumb: "100x100#" }
+  validates_attachment_content_type :avatar,
+                                      content_type: ["image/jpg","image/jpeg","image/png"]
 end
