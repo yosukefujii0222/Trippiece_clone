3dc8f0138803fdbb0de03fb441fc92f00d37df45
yosukefujii0222 <look.in.my.eyes@softbank.ne.jp>
2017-10-12 15:27:51 +0900

    Edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root 'recommends#index'
-  resources :recommends, only: [:index, :new, :create, :edit, :delete]
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: "users/sessions",
+    passwords: "users/passwords"
+  }
+  root to: 'recommends#index'
+  resources :users, only: :show
+  resources :recommends, only: [:index, :new, :create, :show, :edit, :delete]
 end
