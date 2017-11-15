8697ecf6cdff238aa1084a53076146c130b27bd3
yosukefujii0222 <look.in.my.eyes@softbank.ne.jp>
2017-11-15 19:19:08 +0900

    for just only to use production environment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_action :authenticate_user!
   protect_from_forgery with: :exception
   # deviseのストロングパラメータに追加の記述
   before_action :configure_permitted_parameters, if: :devise_controller?
